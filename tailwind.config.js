40ab433da5a772754cd2672ca33d5173eb2e63c0
Sevelar <42201808+Sevelar@users.noreply.github.com>
2022-07-06 21:57:19 +0200

    Create three sections of the page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        background: "url('../public/img/background.jpg')",
+      },
+      backgroundColor: {
+        darkmask: "rgba(0, 0, 0, 0.8)",
+        "new-blurple": "#7289DA",
+        "old-blurple": "#5E6EA8",
+        astron: "#333333",
+      },
+      fontFamily: {
+        saira: "Saira",
+        sans: ["Nunito"],
+      },
+      fontSize: {
+        "5xl": "54px",
+      },
+    },
   },
   plugins: [],
 };
